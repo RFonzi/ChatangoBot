ad8eaf041ef372cc215d032f53356b5cb50344e9
RFonzi <ryan.fonzi@gmail.com>
2015-07-12 16:34:22 -0400

    Readable input stream

--- src/io/github/rfonzi/chatangobot/ChatangoConnection.java
+++ src/io/github/rfonzi/chatangobot/ChatangoConnection.java
@@ -1,16 +1,10 @@
 package io.github.rfonzi.chatangobot;
 
-import sun.nio.ch.SocketAdaptor;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,12 +13,9 @@ public class ChatangoConnection {
     private String login = ""; //Login goes here
     private String password = ""; //Password goes here
     private String loginURLString = "";
-    private String roomURLString = "";
 
     URL loginURL;
-    URL roomURL;
     URLConnection loginConnection;
-    URLConnection roomConnection;
     Socket socket;
 
     public ChatangoConnection() {
@@ -55,12 +46,15 @@ public class ChatangoConnection {
     }
 
     public void joinRoom(String room) throws IOException {
+        socket = new Socket("s65.chatango.com", 443); // Need to remove the need to hardcode this
 
-        socket = new Socket("s65.chatango.com", 443);
-        String tempRoomCode = "bauth:slixtest:1689462054678210:" + this.login + ":" + this.password + "\0";
+        String tempRoomCode = "bauth:" + room + "::" + this.login + ":" + this.password + "\0";
         OutputStream out = socket.getOutputStream();
-        out.write("v\0".getBytes());  //Might not need this
+        //out.write("v\0".getBytes());  //Might not need this
         out.write(tempRoomCode.getBytes());
+
+        out.flush();
+
     }
 
     private void rebuildURL() {
