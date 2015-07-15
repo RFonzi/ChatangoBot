14ebcd135c614565a71d61c7850a0683a15aa631
RFonzi <ryan.fonzi@gmail.com>
2015-07-15 19:19:40 -0400

    Add font constants

---
+++ src/io/github/rfonzi/chatangobot/chatango/Fonts.java
@@ -0,0 +1,13 @@
+package io.github.rfonzi.chatangobot.chatango;
+
+public class Fonts {
+    public static final String ARIAL = "";
+    public static final String COMIC = "1";
+    public static final String GEORGIA = "2";
+    public static final String HANDWRITING = "3";
+    public static final String IMPACT = "4";
+    public static final String PALATINO = "5";
+    public static final String PAPYRUS = "6";
+    public static final String TIMES = "7";
+    public static final String TYPEWRITER = "8";
+}
