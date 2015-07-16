1ea2c2f7012d57cb66bbe410ab294a4d96900f80
RFonzi <ryan.fonzi@gmail.com>
2015-07-16 01:45:06 -0400

    add message sender

---
+++ src/io/github/rfonzi/chatangobot/chatango/MessageSender.java
@@ -0,0 +1,35 @@
+package io.github.rfonzi.chatangobot.chatango;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MessageSender{
+
+
+    private static MessageSender instance = new MessageSender();
+    private SocketInstance socketInstance = SocketInstance.getInstance();
+    private OutputStream out = null;
+
+    public static MessageSender getInstance(){
+        return instance;
+    }
+
+    public void send(String messageCode){
+        if(this.out == null){
+            try {
+                this.out = this.socketInstance.socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            out.write(messageCode.getBytes());
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
