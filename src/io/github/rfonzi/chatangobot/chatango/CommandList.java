eb7a9f495d21e418e90bbb539a387df690a60646
RFonzi <ryan.fonzi@gmail.com>
2015-07-18 01:53:06 -0400

    Added /roll

--- src/io/github/rfonzi/chatangobot/chatango/CommandList.java
+++ src/io/github/rfonzi/chatangobot/chatango/CommandList.java
@@ -23,6 +23,35 @@ enum CommandList implements ICommands {
             return false;
 
         }
+    },
+    ROLL {
+        @Override
+        public void doAction(Message message) {
+            Random random = new Random();
+
+            int roll = random.nextInt(100) + 1;
+
+            if(roll == 22){
+                messageBuilder.insertFontTag("12", "2D3", "Courier New", "@" + message.getSender() + " rolled 22 " + " \rhttp://i.imgur.com/PWs2oSO.png");
+            } else if (roll % 11 == 0 || roll == 100){
+                messageBuilder.insertFontTag("12", "2D3", "Courier New", "@" + message.getSender() + " rolled " + roll + " \rhttp://i.imgur.com/IzO1LYs.png");
+            } else {
+                messageBuilder.insertFontTag("12", "2D3", "Courier New", "@" + message.getSender() + " rolled " + roll);
+            }
+
+            messageSender.send(messageBuilder.toString());
+            messageBuilder.message.clear();
+        }
+
+        @Override
+        public boolean conditions(String messageText) {
+            if (messageText.toLowerCase().startsWith("/roll")) {
+                System.out.println(">> got roll command");
+                return true;
+            }
+
+            return false;
+        }
     };
 
     public static MessageSender messageSender = MessageSender.getInstance();
