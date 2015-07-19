d9962112d704e00e0713154dafdf72f5c9c703c3
RFonzi <ryan.fonzi@gmail.com>
2015-07-19 05:39:56 -0400

    Now supports grabbing a modlist. Bot must be a mod for it to get everything.

--- src/io/github/rfonzi/chatangobot/chatango/PacketTranslator.java
+++ src/io/github/rfonzi/chatangobot/chatango/PacketTranslator.java
@@ -1,6 +1,8 @@
 package io.github.rfonzi.chatangobot.chatango;
 
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static io.github.rfonzi.chatangobot.chatango.Message.Code.*;
 
@@ -9,13 +11,15 @@ public class PacketTranslator implements Runnable {
     PacketQueue packetQueue;
     private String workingString;
     private MessageQueue messageQueue;
+    private RoomInfo roomInfo;
     State state;
 
     public PacketTranslator() {
         packetQueue = PacketQueue.getInstance();
         messageQueue = MessageQueue.getInstance();
-
         state = State.getInstance();
+        roomInfo = RoomInfo.getInstance();
+
 
         state.translatorThread = new Thread(this);
         state.translatorThread.start();
@@ -27,14 +31,30 @@ public class PacketTranslator implements Runnable {
         while (!state.translatorThread.isInterrupted()) {
             try {
                 workingString = packetQueue.queue.poll(5, TimeUnit.SECONDS);
-                if(workingString == null){
+                if (workingString == null) {
                     continue;
                 }
             } catch (InterruptedException e) {
                 break;
             }
 
-            if (workingString.startsWith("b:")) {
+            if (workingString.startsWith("ok:")) { //Get mod list
+
+                Matcher matcher = Pattern.compile("(?<=:)\\w+").matcher(workingString);
+
+                matcher.find();
+                roomInfo.modList.add(matcher.group()); //Add owner
+
+                matcher = Pattern.compile("\\w+(?=,)").matcher(workingString);
+
+                while (matcher.find()) {
+
+                    roomInfo.modList.add(matcher.group()); //Add other mods
+
+                }
+            }
+
+            if (workingString.startsWith("b:")) { //Get message
                 //i:|timestamp|:|handle|::|first 8 digits of magic number!|:|odd string ending in ==|::0:<f x11="0">|message| #0D #0A #00
                 String[] workingStringSplit = workingString.split(":");
                 System.out.println(">> " + workingString);
